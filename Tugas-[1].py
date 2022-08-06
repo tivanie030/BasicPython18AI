05066051b3f36f7bade2c521a5a269a5cd29c587
tivanie030 <110229188+tivanie030@users.noreply.github.com>
2022-08-07 00:24:49 +0700

    Add files via upload

---
+++ Tugas-[1].py
@@ -0,0 +1,17 @@
+nama = input("Siapakah nama Anda? ")
+print("Nama Saya adalah " + nama)
+
+umur = input("Berapakah umur Anda? ")
+print("Umur Saya " + umur)
+
+tinggi = input("Berapakah tinggi Anda? ")
+print("dan tinggi saya " + tinggi)
+
+text = "Nama saya {}, umur saya {}, dan tinggi saya {}"
+print(text.format(nama, umur, tinggi))
+
+
+
+
+
+
